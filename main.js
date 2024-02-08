94f78793062f6096973e9f9350627b9ed6e68fd6
poornani11 <poorani.mgr@gmail.com>
2024-02-08 19:21:31 +0000

    Fixing the footer

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 
 navToggle.addEventListener('click', () => {
-    primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute('data-overlay')
+  primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute('data-overlay')
 })
 
 const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: false,
-    dots: true,
-    centerMode: true,
-    arrows: false,
-    responsive: {
-        480: {
-            dots: false,
-            arrows: true
-        },
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false,
+      arrows: true
     },
+  },
 });
 
 
